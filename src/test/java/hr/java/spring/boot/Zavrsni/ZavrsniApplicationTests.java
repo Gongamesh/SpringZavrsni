ad000b88cfed39a13a223fa4a7d2e3060c33b11b
Gongamesh <kovacevicstjepan02@gmail.com>
2026-07-06 18:41:27 +0200

    Initial commit

---
+++ src/test/java/hr/java/spring/boot/Zavrsni/ZavrsniApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.java.spring.boot.Zavrsni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZavrsniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
